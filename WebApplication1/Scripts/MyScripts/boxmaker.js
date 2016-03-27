e123ac3676b8d6e3ec1c6e72e422bbd213b8d593
KyleHersey <herseykyle@yahoo.com>
2016-03-27 15:19:19 -0500

    Added mergesort
    Separated sorting to sort.js
    Added mergesort

--- WebApplication1/Scripts/MyScripts/boxmaker.js
+++ WebApplication1/Scripts/MyScripts/boxmaker.js
@@ -7,11 +7,17 @@
     }
 
     function makeBox() {
-        for (var i = 0; i < 100; i++) {
+        while (document.getElementById("sandbox").childElementCount > 0) {
+            document.getElementById("sandbox").removeChild(document.getElementById("sandbox").childNodes[0]);
+        }
+
+
+        setupSandbox();
+        for (var i = 0; i < document.getElementById("numBoxes").value; i++) {
             var box = document.createElement("div");
             box.className = "box";
             box.style.marginRight = "1px";
-            box.style.width = "1px";
+            box.style.width = "3px";
             box.style.height = Math.floor(Math.random() * 200) + 1 + "px";
             box.style.backgroundColor = "Blue";
             box.style.float = "left";
@@ -19,64 +25,13 @@
         }
     }
 
-    function sortBoxes(){
-        var children = document.getElementById("sandbox").children;
-        var boxes = [];
-        for (var i = 0; i < children.length; i++){
-            if (children[i].className = "box") {
-                boxes.push(children[i]);
-            }
-        }
-
-        var sorted = false;
-        var intervalDuration = 1;
-        var loopCount = boxes.length;
-        var outerCount = 0;
-        var innerCount = 0;
-        var innerIntervalID = 0;
-        
-        outerLoop();
-        var outerInteravalID = setInterval(outerLoop, intervalDuration*boxes.length + 1);
-
-        function outerLoop() {
-            if (sorted) {
-                clearInterval(outerInteravalID);
-            } else {
-                sorted = true;
-                innerCount = 0;
-                innerLoop();
-                innerIntervalID = setInterval(innerLoop, intervalDuration);
-            }
-        }
-
-        function innerLoop() {
-            
-            console.log(innerCount + "inner");
-
-            if (innerCount >= loopCount - 1) {
-                clearInterval(innerIntervalID);
-            } else {
-                boxes[innerCount].style.backgroundColor = "Yellow";
-
-                var box1Height = parseInt(boxes[innerCount].style.height, 10);
-                var box2Height = parseInt(boxes[innerCount + 1].style.height, 10);
-
-                if (box1Height < box2Height) {
-                    sorted = false;
-                    console.log("swapped");
-                    boxes[innerCount].style.backgroundColor = "Green";
-                    boxes[innerCount + 1].style.backgroundColor = "Red";
-
-                    //BELOW TO BE SWAPDIVS METHOD
-                    //switch locations visibly
-                    var tempStyle = boxes[innerCount].style.height;
-                    boxes[innerCount].style.height = boxes[innerCount + 1].style.height;
-                    boxes[innerCount + 1].style.height = tempStyle;
-                }
-
-                boxes[innerCount].style.backgroundColor = "Blue";
-                boxes[innerCount + 1].style.backgroundColor = "Blue";
-                innerCount++;
-            }
-        }
+    function makebox(height) {
+        var box = document.createElement("div");
+        box.className = "box";
+        box.style.marginRight = "1px";
+        box.style.width = "3px";
+        box.style.height = height + "px";
+        box.style.backgroundColor = "Blue";
+        box.style.float = "left";
+        document.getElementById("sandbox").appendChild(box);
     }
---
+++ WebApplication1/Scripts/MyScripts/sort.js
@@ -0,0 +1,149 @@
+﻿function getBoxArray() {
+    var children = document.getElementById("sandbox").children;
+    var boxes = [];
+    for (var i = 0; i < children.length; i++) {
+        if (children[i].className = "box") {
+            boxes.push(children[i]);
+        }
+    }
+    return boxes;
+}
+
+function getBoxHeights() {
+    var boxes = getBoxArray();
+    var heights = [];
+    for (var i = 0; i < boxes.length; i++) {
+        heights.push(parseInt(boxes[i].style.height));
+    }
+    return heights;
+}
+
+
+function performQueue(instructionQueue){
+    var boxes = getBoxArray();
+
+    var instruction;
+
+    var intervalID = setInterval(doInstruction, 1);
+
+    function doInstruction() {
+        if (instructionQueue.length <= 0) {
+            return;
+        }
+        instruction = instructionQueue.shift()
+        var index1 = instruction.var1;
+        var index2 = instruction.var2;
+
+        doSwap(index1, index2);
+        while(instructionQueue.length > 0 && !instructionQueue[0].takesTime){
+            doInstruction();
+        }
+
+        if (instructionQueue.length == 0) {
+            clearInterval(intervalID);
+        }
+    }
+
+    function doSwap(i1, i2){
+        var tempHeight = boxes[i1].style.height;
+        boxes[i1].style.height = boxes[i2].style.height;
+        boxes[i2].style.height = tempHeight;
+    }
+}
+
+
+function mergeSortBoxesHelper() {
+    var heights = getBoxHeights();
+    var swapQueue = [];
+    
+    console.log(heights);
+
+    var sorted = mergeSortBoxes(0, heights);
+    function mergeSortBoxes(left, heights) {
+        if (heights.length < 2) {
+            return heights;
+        } else {
+            var mid = Math.floor(heights.length / 2);
+            var list1 = mergeSortBoxes(left, heights.slice(0, mid));
+            var list2 = mergeSortBoxes(left + mid, heights.slice(mid, heights.length));
+            return merge(left, list1, list2);
+        }
+    }
+    function merge(left, list1, list2) {
+        var combined = list1.concat(list2);
+
+        var index1 = 0;
+        var index2 = list1.length;
+
+        while (index1 < combined.length && index2 < combined.length) {
+            if (combined[index1] >= combined[index2]) {
+                var i;
+                for (i = index2; i > index1; i--) {
+                    var swap = {var1: i + left, var2: i-1 + left, takesTime: false}
+                    swapQueue.push(swap);
+
+                    var temp = combined[i];
+                    combined[i] = combined[i - 1];
+                    combined[i - 1] = temp;
+                }
+                var pauseSwap = { var1: 0, var2: 0, takesTime: true }
+                swapQueue.push(pauseSwap);
+
+                index2++;
+                index1++;
+            } else {
+                index1++;
+            }
+        }
+        
+        return combined;
+    }
+
+    console.log(sorted);
+
+    performQueue(swapQueue);
+}
+
+
+
+function insertionSortBoxes() {
+    var heights = getBoxHeights();
+
+    var swapQueue = [];
+    for (var index = 1; index < heights.length; index++) {
+        var current = heights[index];
+        for (var sortedIndex = index - 1; sortedIndex >= 0 && heights[sortedIndex] > current; sortedIndex--) {
+            var swap = { var1: sortedIndex, var2: sortedIndex + 1, takesTime: true};
+            swapQueue.push(swap);
+            
+            var temp = heights[sortedIndex];
+            heights[sortedIndex] = heights[sortedIndex + 1];
+            heights[sortedIndex + 1] = temp;
+        }
+    }
+    performQueue(swapQueue);
+}
+
+
+function bubbleSortBoxes() {
+    var heights = getBoxHeights();
+
+    sorted = false;
+    var swapQueue = [];
+    while (!sorted) {
+        sorted = true;
+        for (var i = 0; i < heights.length - 1; i++) {
+            if (heights[i] > heights[i + 1]) {
+                sorted = false;
+
+                var swap = { var1: i, var2: i + 1, takesTime: true};       //swap object to be put on queue
+                swapQueue.push(swap);
+
+                var temp = heights[i];
+                heights[i] = heights[i + 1];
+                heights[i + 1] = temp;
+            }
+        }
+    }
+    performQueue(swapQueue);
+}
